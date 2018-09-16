7156c9d9fb03d244f48258d5f8af90e38adfeb59
efalicov <efalicov@hmc.edu>
2018-09-16 02:54:10 -0400

    more edits to code
    almost working .tm

--- CloudVisionText.py
+++ CloudVisionText.py
@@ -8,11 +8,14 @@ from google.cloud.vision import types
 from PIL import Image, ImageDraw
 from group_word import*
 
-
 import base64
+import enchant
+
+
+space_x = 10
+space_y = 10
 
 def annotate_text(filein):
-
 	client = vision.ImageAnnotatorClient()
 
 	with open(str(filein), "rb") as imageFile:
@@ -21,10 +24,80 @@ def annotate_text(filein):
 	image = vision.types.Image(content = content)
 	resp = client.text_detection(image = image)
 	
-	for d in resp.text_annotations[1:]:
-		word = Group_Word(d.description, d.bounding_poly.vertices[0].x, d.bounding_poly.vertices[0].y, d.bounding_poly.vertices[2].x - d.bounding_poly.vertices[0].x, d.bounding_poly.vertices[2].y - d.bounding_poly.vertices[0].y)
+	output = []
 
-		print(word.toString())
+	for d in resp.text_annotations[1:]:
+		word = Group_Word(d.description, d.bounding_poly.vertices[0].x, d.bounding_poly.vertices[0].y, d.bounding_poly.vertices[2].x - d.bounding_poly.vertices[0].x, d.bounding_poly.vertices[2].y - d.bounding_poly.vertices[0].y, 0)
+
+		output.append(word)
+
+	return output
+
+def determine_math(word_array):
+	dict = enchant.Dict("en_US")
+	
+
+	for word in word_array:
+
+		word_dictionary_description = word.get_description().translate({ord(c): None for c in '.?,;:!`()%'}).lower()
+
+		if (word_dictionary_description != '' and dict.check(word_dictionary_description)):
+			if (verify_helper(word_dictionary_description)):
+				word.set_math(1)
+		else: 
+			word.set_math(1)
+
+def verify_helper(string):
+	if string in {"n", "x", "y", "z", "k", "p", "q", "b", "c", "e", "f", "g", "m"} or string.isnumeric():
+		return True
+	else:
+		return False
+
+def make_lines(word_array, img_width, img_height):
+	return_val = [[word_array[0]]]
+	for i in range(len(word_array) - 2):
+		word = word_array[i]
+		next_word = word_array[i + 1]
+
+		valid_merge = word.should_merge_lines(next_word, space_y, img_height)
+		#print (word.toString(), next_word.toString(), valid_merge)
+
+		if valid_merge[1] == False: #on different lines
+			return_val.append([next_word])
+		else: #math or text match, 
+			return_val[-1].append(next_word)
+	return return_val
+
+	# for line in return_val:
+	# 	print ("[")
+	# 	for line_word in line:
+	# 		print(line_word.toString())
+	# 	print("]")
+
+def condense_lines (word_array, img_height):
+
+	return_val = [word_array[0][0]]
+
+	for line in word_array:
+		if (len(line) <= 1):
+			return_val.append(line[0])
+		else:
+			for i in range(len(line) - 2):
+				next_word = line[i+1]
+				if line[i].should_merge_lines(next_word, space_y, img_height)[0] == True: #line characters all same
+					return_val[-1] = return_val[-1].merge(next_word)
+				else if line[i].should_merge_lines(next_word, space_y, img_height)[0] == False and line[i].should_merge_lines(next_word, space_y, img_height)[1] == True:
+					
+				else:
+				 	return_val.append(next_word)
+	for line in return_val:
+		print(line.toString())
+	print("END")
+
+def make_paragraph():
+	return 0
+
+
 
 
 if __name__ == '__main__':
@@ -35,7 +108,20 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     parser = argparse.ArgumentParser()
-    annotate_text(args.detect_file)
+    image_to_process = args.detect_file
+
+    word_array = annotate_text(image_to_process)
+    determine_math(word_array)
+
+    width, height = Image.open(image_to_process).size
+
+    lines_array = make_lines(word_array, width, height)
+    condense_lines(lines_array, height)
+
+    '''for d in word_array:
+    	print(d.toString())'''
+
+
     # [END vision_document_text_tutorial_run_application]
 # [END vision_document_text_tutorial]
 
--- group_word.py
+++ group_word.py
@@ -1,37 +1,53 @@
 
 class Group_Word:
-	def __init__(self, description = "", x = 0, y = 0, width = 0, height = 0):
+	def __init__(self, description = "", x = 0, y = 0, width = 0, height = 0, is_math = 0):
 		self.DESCRIPTION = description
 		self.X = x
 		self.Y = y
 		self.WIDTH = width
 		self.HEIGHT = height
+		self.IS_MATH = is_math
 
 	def toString(self):
-		return [self.DESCRIPTION, self.X, self.Y, self.WIDTH, self.HEIGHT]
+		return [self.DESCRIPTION, self.X, self.Y, self.WIDTH, self.HEIGHT, self.IS_MATH]
 
+	def get_description(self):
+		return self.DESCRIPTION
+
+	def set_description(self, description):
+		self.DESCRIPTION = description
+
+	def set_math(self, is_math):
+		self.IS_MATH = is_math
 
 	def merge(self, other_word):
-		return group_word
-		[	self.DESCRIPTION + " " + other_word.DESCRIPTION, 
+		new_word = Group_Word(
+			self.DESCRIPTION + " " + other_word.DESCRIPTION, 
 			min(self.X, other_word.X), 
 			min(self.Y, other_word.Y), 
 			max(self.X, other_word.X) - min(self.X, other_word.X),
-			max(self.Y, other_word.Y) - min(self.Y, other_word.Y)]
+			max(self.Y, other_word.Y) - min(self.Y, other_word.Y), 
+			self.IS_MATH)
+		return new_word
 
-	def should_merge(self, other_word, space_x, space_y, img_width, img_height):
-		horizontal = False
+	def should_merge_lines(self, other_word, space_y, img_height):
+		
+		#horizontal = False
 		vertical = False
 
-		horizontal_diff = max(self.X, other_word.X) - min(self.X, other_word.X)
-		horizontal_diff = int(horizontal_diff / img_width * 100) 
+		#horizontal_diff = max(self.X, other_word.X) - min(self.X, other_word.X)
+		#horizontal_diff = abs(other_word.X - self.X)
+		#horizontal_diff = (img_width -horizontal_diff) / img_width 
 
-		horizontal = (horizontal_diff < space_x)
+		#horizontal = (horizontal_diff <= space_x)
 
 
-		vertical_diff = max(self.Y, other_word.Y) - min(self.Y, other_word.Y)
-		vertical_diff = int(vertical_diff / img_height * 100) 
+		vertical_diff = abs(self.Y - other_word.Y)
+		#vertical_diff = max(self.Y, other_word.Y) - min(self.Y, other_word.Y)
+		#vertical_diff = (img_height - vertical_diff) / img_height 
 
-		vertical = (vertical_diff < space_y)
+		vertical = (vertical_diff <= space_y)
+		math_merge = not((self.IS_MATH and not other_word.IS_MATH) or (not self.IS_MATH and other_word.IS_MATH))
 
-		return [horizontal, vertical]
+		#return [horizontal and math_merge, vertical and math_merge] #Ensures math merges with math and non-math merges with non-math
+		return [math_merge, vertical]
